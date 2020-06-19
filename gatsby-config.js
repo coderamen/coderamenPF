0ed70cd9fe7b2f04f6b8d29ce5fe985ec047b91d
coderamen <spencerteh@gmail.com>
2020-06-19 13:38:35 +0800

    install fontawesome and title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Spencer Teh`,
+    title_short: `steh`,
+    author: `Spencer's personal website`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
